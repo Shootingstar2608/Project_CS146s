a136270542eb958564109a26ee95769efc638b94
Timmiethy <lethanhtan101007@gmail.com>
2026-05-08 10:40:09 +0700

    db/db-helper-functions

--- backend/app/api/upload.py
+++ backend/app/api/upload.py
@@ -1,1 +1,15 @@
-from fastapi import APIRouter
+from ast import List
+from fastapi import APIRouter, UploadFile, Depends
+from starlette.status import HTTP_201_CREATED
+from backend.app.models.schemas import UploadResponse
+from models import Document
+from sqlalchemy.ext.asyncio import AsyncSession
+from core import get_db
+
+router = APIRouter(prefix="/upload", tags=["upload"])
+
+
+@router.post("/", response_model=UploadResponse, status_code=HTTP_201_CREATED)
+def upload(files: List[UploadFile], db: AsyncSession = Depends(get_db)):
+    for f in files:
+        pass
